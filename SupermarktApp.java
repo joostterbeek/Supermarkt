16fca32ef51c6be039bcf5bcf430cc8ebcbe3b71
joostterbeek <joostterbeek@hotmail.com>
2019-03-15 11:09:53 +0100

    tweede foto supermarkt

--- SupermarktApp.java
+++ SupermarktApp.java
@@ -2,25 +2,19 @@
 public class SupermarktApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welkom bij de Albert Heijn");
 		Supermarkt albertheijn = new Supermarkt("Professor Bromstraat");
 		albertheijn.openen();
-		albertheijn.openen();
-		Product kaas = new Product("Kaas");
-		Product worst = new Product("Worst");
-		Product pizza = new Product("Pizza");
-		albertheijn.hetProduct1 = new Product("Kaas");
-		albertheijn.hetProduct2 = new Product("Worst");
-		albertheijn.hetProduct3 = new Product("Pizza");
-		albertheijn.hetProduct1.prijs = 230;
-		albertheijn.hetProduct2.prijs = 160;
-		albertheijn.hetProduct3.prijs = 430;		
+		albertheijn.inkopen();
 		albertheijn.assortimentTonen();
 	}
 
 }
 class Supermarkt{
 	String supermarktstraatnaam;
+	Product hetProduct1;
+	Product hetProduct2;
+	Product hetProduct3;
+	
 	Supermarkt(String straatnaam){
 		supermarktstraatnaam = straatnaam;
 	}
@@ -30,19 +24,20 @@ class Supermarkt{
 	void assortimentTonen(){
 		System.out.println("Wij van de Albert Heijn " + supermarktstraatnaam + " hebben " + hetProduct1.deNaam + " " + hetProduct2.deNaam + " " + hetProduct3.deNaam);
 	}
-	void productKopen(Klant k) {
-		
+	void inkopen() {
+		hetProduct1 = new Product("Kaas", 230);
+		hetProduct2 = new Product("Worst", 160);
+		hetProduct3 = new Product("Pizza", 430);
 	}
-	Product hetProduct1;
-	Product hetProduct2;
-	Product hetProduct3;
+	void productKopen(Klant k) {	
+	}
 }
 class Product{
+	int prijsProduct;
 	String deNaam;
-	int prijs;
-	Product(String productNaam){
-		System.out.println(productNaam);
+	Product(String productNaam, int prijs){
 		deNaam = productNaam;
+		prijsProduct = prijs;
 	}
 }
 class Klant{
